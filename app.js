6750a12a06d4cd5c01fd520a36d055904c3b8859
piyushpoudyal <72690317+piyushpoudyal@users.noreply.github.com>
2021-12-01 17:39:45 +0530

    bug: fixed app.js

--- app.js
+++ app.js
@@ -1,40 +1,39 @@
-const billAmount= document.querySelector("#bill-amount");
-const cashGiven=document.querySelector("#cash-given")
-const checkButton= document.querySelector("#check-button");
+const billAmount=document.querySelector("#bill-amount");
+const cashGiven=document.querySelector("#cash-given");
+const checkButton=document.querySelector("#check-button");
 const message=document.querySelector("#error-message");
-const noOfNotes=document.querySelectorAll(".no-of-notes")
+const noOfNotes=document.querySelectorAll(".no-of-notes");
 
-const availableNotes= [2000,500,100,50,10,5,1];
-checkButton.addEventListener("click",function validateAmount(){
- message.style.display="none"
 
-if(cashGiven.value && billAmount.value>=0) {
-        if(cashGiven.value-billAmount.value>=0){
-         if(cashGiven.value>=billAmount.value){
-         const amountReturn=cashGiven.value-billAmount.value;
-         calculateChange(amountReturn);
-        }
-        else{
-            message.style.display="block";
-            message.innerHTML="wanna wash plates?";  
-        }
-    }
-    else{
-message.style.display="block";
-message.innerHTML="wanna wash plates!"; 
-    }}else{
-        message.style.display="block";
-        message.innerHTML="Error amount!";  
-    }
-
-    cashGiven.value='';
-    billAmount.value=''
-})
-
-function calculateChange(amountReturn){
-for(let i=0;i<availableNotes.length;i++){
-    const numberOfNotes =Math.trunc(amountReturn/availableNotes[i]);
-    amountReturn%=availableNotes[i];
- noOfNotes[i].innerText=numberOfNotes;
+const availableNotes=[2000,500,100,20,10,5,1];
+checkButton.addEventListener("click",function calculateChange(){
+hideMessage();
+if(Number(billAmount.value)>0){
+if(Number(cashGiven.value)>=Number(billAmount.value)){
+const amountToBeReturned=cashGiven.value-billAmount.value;
+changeToBeReturned(amountToBeReturned);
+}else{
+    showMessage("wanna wash plates??")
 }
+}else{
+    showMessage("invalid bill amount")
+}
+billAmount.value='';
+cashGiven.value=''
+
+});
+function changeToBeReturned(amountToBeReturned){
+    for(let i=0;i<availableNotes.length;i++){
+        const change=Math.trunc(amountToBeReturned/availableNotes[i]);
+        amountToBeReturned=amountToBeReturned%availableNotes[i]
+        noOfNotes[i].innerText=change;
+    }
+   
+}
+function hideMessage(){
+    message.style.display="none";
+}
+function showMessage(msg){
+    message.style.display="block";
+    message.innerText=msg;
 }
